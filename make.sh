eacfb04eba9e6ce9038e98725a4ce1458cea90e2
wh0dat <whodat@remainsilent.net>
2019-01-26 09:36:56 -0600

    Update build script

--- make.sh
+++ make.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ ! -f certs/signing_key.pem ]; then
+	echo "Generating new key."
+	openssl req -x509 -newkey rsa:4096 -keyout key.pem -out certs/signing_key.pem -days 9999
+fi
+export USER=build
 make \
 ARCH=arm \
 CROSS_COMPILE=gcc-linaro-7.1.1/bin/arm-linux-gnueabihf- \
